10bedd65d2aeb8ae7eebd63e9bd7ca984ce771bc
fredmaya <fredericomaya@icloud.com>
2024-04-16 18:47:15 -0700

    refreshing pages

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,24 @@
 import "./Header.scss";
-import { NavLink, useNavigate, Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function Header() {
-  const navigate = useNavigate();
   // Function to force refresh the page
   const handleFormClick = () => {
     window.location.reload(); // Force a page reload
   };
-  const navigateToMainPage = () => {
-    navigate("/"); // Navigate to the main page
-  };
+
   return (
     <header>
       <nav className="navbar">
         <div className="navbar__logo">
-          {/* Wrap the h2 inside a div */}
-          <div className="nav-link" onClick={navigateToMainPage}>
-            <h2>Wine Me Up!</h2>
-          </div>
+          {/* Call handleFormClick when the link is clicked */}
+          <p className="" onClick={handleFormClick}>
+            <NavLink to="/" className="nav-link">
+              Wine Me Up!
+            </NavLink>
+          </p>
         </div>
+
         <div className="navbar__links">
           {/* Call handleFormClick when the link is clicked */}
           <p className="" onClick={handleFormClick}>
