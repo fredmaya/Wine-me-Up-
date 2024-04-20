b7718992669824461739d2ef2dc531b052fc3e72
fredmaya <fredericomaya@icloud.com>
2024-04-20 07:50:24 -0700

    prompt

--- src/components/PromptCards/PromptCards.jsx
+++ src/components/PromptCards/PromptCards.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import "./ChatGPTCards.scss";
+import "./PromptCards.scss";
 
 function ChatGPTCards() {
   const [wineBrand, setWineBrand] = useState("");
